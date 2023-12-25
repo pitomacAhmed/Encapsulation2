c88a334889a2b135c241eed417213d72b2b07315
pitomacAhmed <148368787+pitomacAhmed@users.noreply.github.com>
2023-12-25 16:19:14 +0100

    Add files via upload

---
+++ Encapsulation 2/src/encapsulation/readonly.java
@@ -0,0 +1,9 @@
+  
+public class readonly{  
+  
+private String college="AKG";  
+ 
+public String getCollege(){  
+return college;  
+}  
+}  
---
+++ Encapsulation 2/src/encapsulation/writeonly.java
@@ -0,0 +1,10 @@
+
+
+public class writeonly{  
+
+private String college;  
+  
+public void setCollege(String college){  
+this.college=college;  
+}  
+}  
